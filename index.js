f2c563df0f4400fb390a03da51334ba12eed88b9
Sriram Velamur <techiev2@users.noreply.github.com>
2025-12-22 02:07:35 +0530

    Adds v1 of API. (#1)
    * Adds WIP workflow.
    
    * Adds readme.
    
    * Adds license.
    
    * Updates readme for function type constraints.
    
    * Updates API to infer caller name.
    
    * Updates readme to reflect window scoping for functions.
    
    * Adds package.json for publishing
    
    * Renames package for namespacing to avoid collision.
    
    * Makes package public.
    
    * Updates license in package.json
    
    * Adds base ignore rules
    
    * Adds function check for registered callbacks.

---
+++ index.js
@@ -0,0 +1,30 @@
+window.addEventListener("online", async (e) => {
+  let queue = JSON.parse(localStorage.getItem('networkQueue') || '[]')
+  let message = "We are back!"
+  if (queue.length) {
+    message = `${message}. Attempting your previous requests.`
+  }
+  showToast(message, 2);
+  const successes = new Set((await Promise.allSettled(queue.map(async ({fn, payload}, idx) => {
+    const func = window[fn]
+    if (!func || typeof func !== 'function') {
+      console.error(`${fn} is not available as a function at the window level.`)
+      return idx
+    }
+    await func(payload)
+    return idx
+  }))).filter((prom) => prom.status === 'fulfilled').map(({ value }) => value))
+  const newQueue = JSON.stringify(queue.filter((_, idx) => !successes.has(idx)))
+  localStorage.setItem('networkQueue', newQueue)
+})
+function addToQueue(key, payload) {
+  let caller
+  try {
+    throw new Error()
+  } catch (err) {
+    caller = err.stack.split('\n')[1].split(' at ')[1].split(' ')[0]
+  }
+  const queue = JSON.parse((localStorage.getItem(key) || '[]'))
+  queue.push({ fn: caller, payload })
+  localStorage.setItem(key, JSON.stringify(queue))
+}

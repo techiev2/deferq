65c056b7665ffa9223ebea7c5c88ba8993f2881b
Sriram Velamur <techiev2@users.noreply.github.com>
2025-12-23 02:35:27 +0530

    [feat] v1.1.3 with fixes to balance safety and DX. (#2)

--- index.js
+++ index.js
@@ -1,54 +1,42 @@
-window && window.addEventListener("online", async (e) => {
-  Object.entries(localStorage).filter(([k]) => {
-    return k.endsWith('Queue')
-  }).map(async ([k, v]) => {
-    let queue = JSON.parse(v)
-    let message = "We are back!"
-    if (queue.length) {
-      message = `${message}. Attempting your previous requests.`
-    }
-    const successes = new Set((await Promise.allSettled(queue.map(async ({ fn, payload }, idx) => {
-      const func = window[fn]
-      if (!func || typeof func !== 'function') {
-        console.error(`${fn} is not available as a function at the window level.`)
-        return idx
-      }
-      await func(payload)
-      return idx
-    }))).filter((prom) => prom.status === 'fulfilled').map(({ value }) => value))
-    const newQueue = JSON.stringify(queue.filter((_, idx) => !successes.has(idx)))
-    localStorage.setItem(k, newQueue)
+let queueData = {}
+let registers = {}
+function register(fn) {
+  const { name } = fn
+  if (!registers[name]) {
+    registers[name] = fn
+  }
+}
+async function addToQueue(queueName, fnName, payload = {}) {
+  const fn = registers[fnName]
+  if (!fn) {
+    return console.error(`Function ${fnName} is not registered. Not allowing unsafe usage.`)
+  }
+  if (typeof fn !== 'function') {
+    return console.error(`${fnName} is not a function. Not allowing unsafe usage.`)
+  }
+  const _registeredQueueCalls = queueData[queueName] || []
+  _registeredQueueCalls.push({
+    fn, payload
   })
-})
-function addToQueue(key, payload) {
-  let caller
-  try {
-    throw new Error()
-  } catch (err) {
-    const errLines = err.stack.split('\n')
-    let thisReached = false
-    errLines.map((line) => {
-      if (line.indexOf('at addToQueue') !== -1) {
-        thisReached = true
-        return
-      }
-      if (thisReached) {
-        if (!caller) {
-          caller = line.split(' at ')[1].split(' ')[0]
-        } else {
-          return
-        }
-      }
-    })
-  }
-  if (caller !== 'addToQueue') {
-    const queue = JSON.parse((localStorage.getItem(key) || '[]'))
-    queue.push({ fn: caller, payload })
-    localStorage.setItem(key, JSON.stringify(queue))
-  }
+  queueData[queueName] = _registeredQueueCalls
 }
 
-export default addToQueue
-if (window) {
-	window.addToQueue = addToQueue
+function setupEvents() {
+  window.addEventListener('online', async () => {
+    await Promise.all(Object.entries(queueData).map(async ([queueName, calls]) => {
+      const remaining = []
+      for (const call of calls) {
+        try {
+          await call.fn(call.payload)
+        } catch {
+          remaining.push(call)
+        }
+      }
+      queueData[queueName] = remaining
+    }))    
+  })
 }
+
+if (window) { window.deferq = { addToQueue, register } }
+setupEvents()
+export { addToQueue, register };
